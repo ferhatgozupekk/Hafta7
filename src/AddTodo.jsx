9f3a58a9a8cd13a9f4169eadd1f521826dfcdca4
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2024-01-29 17:07:28 +0300

    11.6 useReducer Kullanımı

--- src/AddTodo.jsx
+++ src/AddTodo.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useReducer } from "react";
+import { ADD_TODO, initial, todoReducer } from "./reducer/todo";
 
 function AddTodo() {
+  const [state, dispatch] = useReducer(todoReducer, initial);
+  console.log("state" + state);
+  console.log("dispatch" + dispatch);
   function handleSubmit(event) {
     event.preventDefault();
     console.log(event.target.elements[0]);
+    dispatch(ADD_TODO);
   }
   return (
     <>
